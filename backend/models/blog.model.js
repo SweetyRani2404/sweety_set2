3846452ce075e35e97f0ba7debcd25d77c1b0720
sweetyrani2404 <sweetyrani8800@gmail.com>
2025-08-25 15:36:45 +0530

    blogModel

---
+++ backend/models/blog.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    imageURL: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.models.Blog || mongoose.model('Blog', blogSchema);
+export default Blog;
